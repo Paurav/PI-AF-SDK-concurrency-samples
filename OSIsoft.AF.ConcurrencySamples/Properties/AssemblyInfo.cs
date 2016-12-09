5a4ff3e6eb53238ae75a0b228ed970191e806a73
pthivierge <pthivierge@osisoft.com>
2016-12-09 11:15:02 +0100

    Updated Licence

--- OSIsoft.AF.ConcurrencySamples/Properties/AssemblyInfo.cs
+++ OSIsoft.AF.ConcurrencySamples/Properties/AssemblyInfo.cs
@@ -1,11 +1,26 @@
-﻿// Copyright (c) OSIsoft, LLC. All rights reserved. See LICENSE for licensing information.
+﻿#region Copyright
+//  Copyright 2016  OSIsoft, LLC
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+#endregion
+
 
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("OSIsoft.AF.ConcurrencySamples")]
 [assembly: AssemblyCompany("OSIsoft, LLC")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: Guid("8b778595-39da-4574-95ff-8acfa0a44f45")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
